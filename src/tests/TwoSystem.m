2bb3473f833836f39d4def6d60112e00ff5b6f53
LivanKov <ilomakov4@gmail.com>
2026-01-21 04:59:39 +0100

    further adjustements

--- src/tests/TwoSystem.m
+++ src/tests/TwoSystem.m
@@ -1,3 +1,10 @@
 sys = System;
-results = sys.runTest('length', 200000, 'noiseVariance', 0.3, 'pulseShape', Pulse.RRC);
-fprintf('BER improvement: %.4f%%\n', results.improvement);
+% Test without filter
+results1 = sys.runTest('length', 400000, 'useBandpassFilter', false, 'verbose', false);
+
+% Test with filter
+results2 = sys.runTest('length', 400000, 'useBandpassFilter', true, 'verbose', false);
+
+% Compare manually
+disp(results1);
+disp(results2);
